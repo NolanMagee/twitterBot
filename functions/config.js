aa98d3af5439b80589afa4e4d42fe0842d277d40
Nolan Magee <nolanmagee@gmail.com>
2018-08-24 11:40:42 -0700

    Initial Commit

---
+++ functions/config.js
@@ -0,0 +1,23 @@
+
+/*
+Consumer API keys
+LXFdTH8UcS7QujdBHfksBKO8B (API key)
+
+ohEuEStwCoJl7hkjId4VyO3mKTBmxkqhXprbUStkFbPBSrmIIo (API secret key)
+
+Access token & access token secret
+1030925803867885568-LpinE94plFrmgcqaLig9xcmIiYOT5B (Access token)
+
+2WfCoNmK48NXNSQ2g2QnCzvRDmVqUKWSnR6nYG0VqHmDq (Access token secret)
+
+
+
+*/
+
+
+module.exports = {
+  consumer_key: 'LXFdTH8UcS7QujdBHfksBKO8B',
+  consumer_secret: 'ohEuEStwCoJl7hkjId4VyO3mKTBmxkqhXprbUStkFbPBSrmIIo',
+  access_token: '1030925803867885568-LpinE94plFrmgcqaLig9xcmIiYOT5B',
+  access_token_secret: '2WfCoNmK48NXNSQ2g2QnCzvRDmVqUKWSnR6nYG0VqHmDq'
+}
---
+++ functions/index.js
@@ -0,0 +1,49 @@
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+const XMLHTttpRequest = require('xhr2')
+
+
+const twit = require('twit')
+const config = require('./config.js')
+const Twitter = new twit(config)
+
+admin.initializeApp(functions.config().firebase)
+
+exports.showTwitter = functions.https.onRequest((request,response)=>{
+  response.send(Twitter)
+})
+
+const url = 'https://www.reddit.com/r/cryptocurrency/top.json'
+const getJSON = function(url, callback){
+  const xhr = new XMLHttpRequest()
+  xhr.open('GET', url, true)
+  xhr.responseType = 'json'
+  xhr.onload = function(){
+    let status = xhr.status
+    if (status === 200){
+      return callback(null, xhr.response)
+    } else {
+      return callback(status, xhr.response)
+    }
+  }
+  xhr.send()
+}
+
+exports.tweetRedditPost = functions.https.onRequest((req,res)=>{
+  getJSON(url, (err,data)=>{
+    if (err !== null){
+      return res.send(err)
+    } else {
+      return res.send(data)
+    }
+  })
+})

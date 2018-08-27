304ee8794ad53cb006f1af5855b37297708d47a6
Nolan Magee <nolanmagee@gmail.com>
2018-08-27 10:49:13 -0700

    CORS WORKS

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,8 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+const cors = require('cors')({origin: true})
+const express = require('express')
 
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
 
 const XMLHTttpRequest = require('xhr2')
 
@@ -19,23 +14,38 @@ const Twitter = new twit(config)
 admin.initializeApp(functions.config().firebase)
 
 
-//give coordinates in the url
-//e.g. for Vancouver:
-//https://us-central1-twitterbot-77c66.cloudfunctions.net/showTwitter?lat=49.3&lng=-123
-exports.showTwitter = functions.https.onRequest((request,response)=>{
-  let lat = request.query.lat || "-22.912"  //Rio de Janeiro
-  let lng = request.query.lng || "-43.231" //43.231
-  let query = `geocode:${lat},${lng},25km`
-  Twitter.get('search/tweets', {q: query}, (err,data,res)=>{
-    //to be done
-    if (err){
-      response.send("ERROR: ", err)
-    }else{
-      response.send({"lat": lat, "lng": lng, "query": query, "data": data})
-    }
+
+
+  exports.showTwitter = functions.https.onRequest((request,response)=>{
+    cors(request, response, ()=>{})
+
+    let lat = request.query.lat || "-22.912"  //Rio de Janeiro
+    let lng = request.query.lng || "-43.231" //43.231
+    let query = `geocode:${lat},${lng},5km`
+    Twitter.get('search/tweets', {q: query}, (err,data,res)=>{
+      //to be done
+      if (err){
+        response.send("ERROR: ", err)
+      }else{
+        response.send({"lat": lat, "lng": lng, "query": query, "data": data})
+      }
+    })
+
   })
 
-})
+
+
+
+
+
+
+
+
+
+
+/*
+
+
 
 const url = 'https://www.reddit.com/r/cryptocurrency/top.json'
 const getJSON = function(url, callback){
@@ -62,3 +72,5 @@ exports.tweetRedditPost = functions.https.onRequest((req,res)=>{
     }
   })
 })
+
+*/
